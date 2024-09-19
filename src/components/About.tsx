7bc6710833ff953864b13357be3722b7adb8b49e
999Marv <marvinsiri123@gmail.com>
2024-09-19 17:23:00 -0400

    adding about me text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,29 @@
 function About() {
-  return <p className="text-black">Your about me content here.</p>;
+  return (
+    <p className="text-black pt-4">
+      Hi, I'm Marvin! I’m a software engineer based in New York City with a
+      passion for technology and development. During my internship at TodayTix
+      Group, I contributed to launching a rewards system that impacted over
+      500,000 users, focusing on backend development using Groovy and SQL.
+      <br />
+      <br />
+      I'm a quick learner who enjoys taking on new challenges. Currently, I'm
+      writing an interpreter in Go and honing my skills in data structures and
+      algorithms.
+      <br />
+      <br />
+      Beyond tech, I have interests in football, video games, anime, manga,
+      traveling, and movies.
+      <br />
+      <br />
+      I'm actively seeking software engineering roles, including Fullstack,
+      Frontend, Backend, QA, and DevOps.
+      <br />
+      <br />
+      Below are details of my journey, projects I’d like to highlight, and my
+      contact information!
+    </p>
+  );
 }
 
 export default About;
